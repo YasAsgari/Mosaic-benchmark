fe8f69ca680508e85b66a5d5e57860be4881986b
Yasaman Asgari <50579308+YasAsgari@users.noreply.github.com>
2023-08-20 18:06:33 +0000

    modified:   mosaic/mosaic/modular_link_stream.py

--- mosaic/mosaic/modular_link_stream.py
+++ mosaic/mosaic/modular_link_stream.py
@@ -1,56 +1,110 @@
+"""The unifier of all codes in the library"""
 import itertools
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from mosaic.mosaic_community import Mosaic
-from mosaic.edge_generator import outside_temporal_edges,inside_temporal_edges
+from mosaic.edge_generator import outside_temporal_edges, inside_temporal_edges
 from mosaic.visualisation_helper import visualize_mosaics
-class Modular_linkstream:
-    def __init__(self, number_of_nodes:int, t_start: float, t_end: float, sceneraio_description: str=None) :
-        assert t_start>=0,'Starting time should be non-negative'
-        assert t_end> t_start, 'Ending time should be greater than starting time'
-        self.t_end=t_end
-        self.t_start=t_start
-        self.number_of_nodes=number_of_nodes
-        self.number_of_communities=0
-        self.communities={}
-        #if sceneraio_description=='random':
-          #  self.snap_shot_scenario_generator()
-       # elif sceneraio_description=='snapshot':
-        #    self.snap_shot_scenario_generator()
-        
-        self.temporal_edges=[]
 
-    def add_community(self,nodes: list, t_start: float, t_end: float):
-        self.number_of_communities+=1
-        self.communities[f'c{self.number_of_communities}']=Mosaic(nodes, t_start, t_end)
+# Define a class for managing a modular link stream
+class ModularLinkStream:
+    def __init__(self, number_of_nodes: int, t_start: float, t_end: float, scenario_description: str = None):
+        """
+        Initialize the ModularLinkStream class.
 
-    def remove_community(self, label:str):
+        Parameters:
+        - number_of_nodes: Total number of nodes in the network.
+        - t_start: Starting time of the link stream.
+        - t_end: Ending time of the link stream.
+        - scenario_description: Description of the scenario (optional).
+        """
+        # Validate input parameters
+        assert t_start >= 0, 'Starting time should be non-negative'
+        assert t_end > t_start, 'Ending time should be greater than starting time'
+
+        # Initialize attributes
+        self.t_start = t_start
+        self.t_end = t_end
+        self.number_of_nodes = number_of_nodes
+        self.number_of_communities = 0
+        self.communities = {}
+        self.temporal_edges = []
+
+    def add_community(self, nodes: list, t_start: float, t_end: float):
+        """
+        Add a community to the link stream.
+
+        Parameters:
+        - nodes: List of nodes in the community.
+        - t_start: Starting time of the community.
+        - t_end: Ending time of the community.
+        """
+        self.number_of_communities += 1
+        self.communities[f'c{self.number_of_communities}'] = Mosaic(
+            nodes, t_start, t_end)
+
+    def remove_community(self, label: str):
+        """
+        Remove a community from the link stream.
+
+        Parameters:
+        - label: Label of the community to be removed.
+        """
         if label in self.communities:
-            self.number_of_communities-=1
+            self.number_of_communities -= 1
             self.communities.pop(label, None)
-            return
-        raise AssertionError('Label is not present')
-    
-    def generate_edges(self, alpha:float, beta:float, lmbda_in:float, lmbda_out:float):
+        else:
+            raise ValueError('Label is not present')
+
+    def generate_edges(self, alpha: float, beta: float, lambda_in: float, lambda_out: float):
+        """
+        Generate temporal edges between communities and within communities.
+
+        Parameters:
+        - alpha: Parameter for edge generation.
+        - beta: Parameter for edge generation.
+        - lambda_in: Parameter for internal edge generation.
+        - lambda_out: Parameter for external edge generation.
+        """
         self.temporal_edges.clear()
-        
-        for mosaic1 , mosaic2 in itertools.combinations(self.communities.values(),2):
-            self.temporal_edges.extend(outside_temporal_edges(mosaic1,mosaic2, lmbda_out,alpha, beta))
-        
+
+        # Generate edges between different communities
+        for mosaic1, mosaic2 in itertools.combinations(self.communities.values(), 2):
+            self.temporal_edges.extend(outside_temporal_edges(
+                mosaic1, mosaic2, alpha, beta, lambda_out))
+
+        # Generate edges within each community
         for mosaic in self.communities.values():
-            self.temporal_edges.extend(inside_temporal_edges(mosaic,alpha, lmbda_in))
+            self.temporal_edges.extend(
+                inside_temporal_edges(mosaic, alpha, lambda_in))
 
     def clear_edges(self):
+        """Clear the list of temporal edges."""
         self.temporal_edges.clear()
 
-    def export(self, address:str):
-        edge_stream_dataframe=pd.DataFrame(self.temporal_edges, columns=['node1', 'node2','time'])
-        edge_stream_dataframe.to_csv(address+'-edges.csv', index=False)
-        np.save(address+'-communities.npy', self.communities) 
+    def export(self, address: str):
+        """
+        Export the link stream data to files.
 
-    def plot(self, ax=None):
-        if ax==None:
-            fig,ax=plt.subplots(nrows=1, ncols=1, figsize=(8,6), dpi=200)       
-        visualize_mosaics(self.communities, ax)
+        Parameters:
+        - address: Address for exporting data.
+        """
+        # Convert temporal edges to DataFrame and export as CSV
+        edge_stream_dataframe = pd.DataFrame(
+            self.temporal_edges, columns=['node1', 'node2', 'time'])
+        edge_stream_dataframe.to_csv(address + '-edges.csv', index=False)
 
+        # Export communities as a NumPy array
+        np.save(address + '-communities.npy', self.communities)
+
+    def plot(self, axis=None):
+        """
+        Plot the communities using a visualization helper.
+
+        Parameters:
+        - ax: Axes object for plotting (optional).
+        """
+        if axis is None:
+            _, axis = plt.subplots(nrows=1, ncols=1, figsize=(8, 6), dpi=200)
+        visualize_mosaics(self.communities, axis)
